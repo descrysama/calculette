88365c0b4e78a63ccb70a2a91c7cc9879f5b725d
DescrySama <erwing2002@gmail.com>
2022-01-04 12:16:23 +0100

    calcul

---
+++ script.js
@@ -0,0 +1,47 @@
+let num1 = parseInt(prompt("Number 1"));
+let parameter = prompt("Paramètre de calcul ?");
+let num2 = parseInt(prompt("Number 2"));
+
+// IF SECTION
+
+// if (parameter === "+") {
+//     let result = num1 + num2;
+//     document.getElementById("fonction-display").innerHTML = result;
+// } else if (parameter === "-") {
+//     result = num1 - num2;
+//     document.getElementById("fonction-display").innerHTML = result;
+// } else if (parameter === "*") {
+//     result = num1 * num2;
+//     document.getElementById("fonction-display").innerHTML = result;
+// } else {
+//     result = num1 / num2;
+//     document.getElementById("fonction-display").innerHTML = result;
+// }
+
+// IF SECTION
+
+
+// SWITCH SECTION
+
+let result;
+switch (parameter) {
+    case "+" :
+        result = num1 + num2;
+        document.getElementById("fonction-display").innerHTML = result;
+        break;
+    case "-" :
+        result = num1 - num2;
+        document.getElementById("fonction-display").innerHTML = result;
+        break;
+    case "*" :
+        result = num1 * num2;
+        document.getElementById("fonction-display").innerHTML = result;
+        break;
+    case "/" :
+        result = num1 / num2;
+        document.getElementById("fonction-display").innerHTML = result;
+        break;
+}
+
+
+// SWITCH SECTION
